a7ec174aebdb8965fbaa9ddbc1ae6bf9a3772ba7
rajesh-udhayan <rajeshkudhayan@gmail.com>
2022-03-17 19:55:46 +0530

    Welcome UI done with composables

--- app/src/main/java/com/example/tddkata/MainActivity.kt
+++ app/src/main/java/com/example/tddkata/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.example.tddkata
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tddkata.ui.theme.TDDKataTheme
+import com.example.tddkata.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +30,20 @@ class MainActivity : ComponentActivity() {
             TDDKataTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text(text = "Welcome", style = Typography.h3)
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .fillMaxWidth()
+                                .fillMaxHeight(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            TextInputs()
+                            SubmitButton()
+                        }
+                    }
                 }
             }
         }
@@ -25,14 +51,55 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TextInputs() {
+
+    var username by remember { mutableStateOf(TextFieldValue("")) }
+    var password by remember { mutableStateOf(TextFieldValue("")) }
+
+    OutlinedTextField(value = username,
+        leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = null) },
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+        label = { Text(text = "Email address") },
+        onValueChange = {
+            username = it
+        }
+    )
+
+    OutlinedTextField(
+        value = password,
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        label = { Text(text = "Password") },
+        visualTransformation = PasswordVisualTransformation(),
+        onValueChange = {
+            password = it
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+    )
+
+}
+
+@Composable
+fun SubmitButton() {
+    val context = LocalContext.current
+    Button(onClick = {
+        Toast.makeText(context, "Welcome", Toast.LENGTH_LONG).show()
+    }) {
+        Text(text = "Login", color = Color.White)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TDDKataTheme {
-        Greeting("Android")
+        Column {
+            TextInputs()
+            SubmitButton()
+        }
     }
 }
